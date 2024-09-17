369e803ce7eae28e846c0b030fec4ccf68adf6c2
Singadwar Sushanth Reddy <76097402+sushanth1234reddy@users.noreply.github.com>
2024-09-17 22:04:33 +0530

    Add files via upload

---
+++ rightAngledTriangle.js
@@ -0,0 +1,16 @@
+// * 
+// * * 
+// * * * 
+// * * * * 
+// * * * * * 
+let rows=+prompt("Enter No.Of Rows: ");
+for(let i=1;i<=rows;i++){
+    let res="";
+    for(j=1;j<=i;j++){
+        res=res+"* ";
+    }
+    console.log(res);
+}
+// for(let i=1;i<=rows;i++){
+//   console.log("* ".repeat(i)); 
+// }
